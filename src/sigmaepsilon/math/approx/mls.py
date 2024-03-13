3b92182751a2ea71cd8fd2b5ac2512307902d05d
Bence Balogh <benceeok@gmail.com>
2024-03-13 18:35:46 +0100

    added knn

---
+++ src/sigmaepsilon/math/approx/mls.py
@@ -0,0 +1,12 @@
+from numpy import ndarray
+
+from ..knn import k_nearest_neighbours
+
+
+class MLSApproximator():
+    
+    def fit(self, coords: ndarray, values: ndarray):
+        pass
+    
+    def approximate(self, coords: ndarray):
+        pass
