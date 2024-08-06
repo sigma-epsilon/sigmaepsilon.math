ed2370250135b617e31642c1656fc1bf4d83a1a3
Bence Balogh <benceeok@gmail.com>
2024-08-06 18:58:33 +0200

    improved on type hints

--- src/sigmaepsilon/math/function/variablemanager.py
+++ src/sigmaepsilon/math/function/variablemanager.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from typing import Iterable
 
 import sympy as sy
 from sympy import lambdify
@@ -7,20 +8,30 @@ from .metafunction import substitute
 
 
 class VariableManager(object):
-    def __init__(self, variables=None, vmap=None, **kwargs):
+    """
+    A class to manage variables.
+    """
+
+    def __init__(
+        self, variables: Iterable | None = None, vmap: dict | None = None, **kwargs
+    ):
         try:
             variables = list(sy.symbols(variables, **kwargs))
         except Exception:
             variables = variables
+
         try:
             self.vmap = (
                 vmap if vmap is not None else OrderedDict({v: v for v in variables})
             )
         except Exception:
             self.vmap = OrderedDict()
+
         self.variables = variables  # this may be unnecessary
 
-    def substitute(self, vmap: dict = None, inverse=False, inplace=True):
+    def substitute(
+        self, vmap: dict | None = None, inverse: bool = False, inplace: bool = True
+    ):
         if not inverse:
             sval = list(vmap.values())
             svar = list(vmap.keys())
@@ -37,7 +48,7 @@ class VariableManager(object):
                 vmap[v] = substitute(expr, sval, svar)
             return vmap
 
-    def lambdify(self, variables=None):
+    def lambdify(self, variables: Iterable | None = None):
         assert variables is not None
         for v, expr in self.vmap.items():
             self.vmap[v] = lambdify([variables], expr, "numpy")
@@ -45,16 +56,16 @@ class VariableManager(object):
     def __call__(self, v):
         return self.vmap[v]
 
-    def target(self):
+    def target(self) -> list:
         return list(self.vmap.keys())
 
-    def source(self):
+    def source(self) -> list:
         s = set()
         for expr in self.vmap.values():
             s.update(expr.free_symbols)
         return list(s)
 
-    def add_variables(self, variables, overwrite=True):
+    def add_variables(self, variables: Iterable, overwrite:bool=True) -> None:
         if overwrite:
             self.vmap.update({v: v for v in variables})
         else:
