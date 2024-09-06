9e433e59c962ffafa7798dab734ca6532d61008b
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-09-06 12:59:53 +0200

    removed VariableManager class

--- src/sigmaepsilon/math/function/__init__.py
+++ src/sigmaepsilon/math/function/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from .function import Function, FunctionLike
 from .relation import Equality, InEquality, Relation
-from .variablemanager import VariableManager
 
 __all__ = [
     "Function",
     "FunctionLike",
-    "VariableManager",
     "Equality",
     "InEquality",
     "Relation",
