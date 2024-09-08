a767acb4425650e5b4c7e98fb42a400297d4fe12
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-09-08 19:15:27 +0200

    refactored __init__.py

--- src/sigmaepsilon/math/optimize/__init__.py
+++ src/sigmaepsilon/math/optimize/__init__.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
 from .bga import BinaryGeneticAlgorithm
 from .lp import LinearProgrammingProblem
-from .errors import *
+from .simplex_lp import SimplexSolverLP
+from .errors import DegenerateProblemError, NoSolutionError, OverDeterminedError
+
+
+__all__ = [
+    "BinaryGeneticAlgorithm",
+    "LinearProgrammingProblem",
+    "SimplexSolverLP",
+    "DegenerateProblemError",
+    "NoSolutionError",
+    "OverDeterminedError",
+]
