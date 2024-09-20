5d9c2e11c51d6e652fe36062ac89a8239e1ea84d
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-09-20 18:59:34 +0200

    updated config

--- export.sh
+++ export.sh
@@ -3,3 +3,4 @@ poetry export -f requirements.txt --output requirements.txt
 poetry export -f requirements.txt --only dev --output requirements-dev.txt
 poetry export -f requirements.txt --only test --output requirements-test.txt
 poetry export -f requirements.txt --only docs --output ./docs/requirements.txt
+echo "Requirements exported"
