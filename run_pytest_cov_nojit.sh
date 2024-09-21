8ca6df805a93c5d1c23141ee027695a4478d7d29
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-09-21 23:02:16 +0200

    updated

--- run_pytest_cov_nojit.sh
+++ run_pytest_cov_nojit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NUMBA_DISABLE_JIT=1
-poetry run pytest --cov-report=html --cov-config=.coveragerc_nojit --cov sigmaepsilon.math
+poetry run pytest --cov-report=html --cov-config=.coveragerc_nojit --cov=sigmaepsilon.math
 export NUMBA_DISABLE_JIT=0
